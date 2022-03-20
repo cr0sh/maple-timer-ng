027fd9e3a462f7ce56252f1c9506e5bc053b8f90
Nam Junghyun <ska827@snu.ac.kr>
2022-03-20 19:56:36 +0900

    Update package name

--- build_isolated.ps1
+++ build_isolated.ps1
@@ -1,5 +1,5 @@
 $mnt = $args[0].Trim()
 Copy-Item $mnt/* . -Recurse -Force -Exclude $mnt/target
 cargo build --release --features windows_subsystem
-Copy-Item target/release/maple_timer_ng.exe $mnt/maple_timer_ng_release.exe
+Copy-Item target/release/maple_timer.exe $mnt/maple_timer_release.exe
 echo Done
